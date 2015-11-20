735ec035fe18e87373126e97caa3224629b4096b
Александр Киклевич <sashakiklevich@outlook.com>
2015-11-20 20:38:43 +0300

    McCabeJAVA

---
+++ MACCABE_JAVA/Analize.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MACCABE_JAVA
+{
+    class Function
+    {
+        public string Source { get; set; }
+        public string NameOfFunction { get; set;  }
+        public int McCabe { get; set; }
+        public int Meyer { get; set; }
+    }
+    class Analize
+    {
+        public Analize(string src)
+        {
+            DeleteLiterals(ref src);
+            DeleteComments(ref src);
+            Functions = new List<Function>();
+            FindFunctions(src);
+
+        }
+        public List<Function> Functions { get; set; }
+
+        public void DeleteLiterals(ref string src)
+        {
+            Regex find = new Regex("\"(?:\\\\\"|[^\"])*?\"", RegexOptions.IgnoreCase);
+            src = find.Replace(src, "");
+        }
+
+        public void DeleteComments(ref string src)
+        {
+            Regex find = new Regex(@"(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)");
+            src = find.Replace(src, "");
+        }
+
+        private int EndBraketsPosition(string src)
+        {
+            int countEnd = 0;
+            int resultEnd = 0;
+            for (int i = 0; (i < src.Length); i++)
+            {
+                if (src[i] == '{')
+                {
+                    countEnd++;
+                }
+                if (src[i] == '}')
+                {
+                    countEnd--;
+                    if (countEnd == 0)
+                    {
+                        resultEnd = i;
+                        break;
+                    }
+                }
+            }
+            return resultEnd;
+        }
+        private string NextTextInBrakes(string src)
+        {
+            string result = src.Remove(0, src.IndexOf('{') + 1).Remove(1 + EndBraketsPosition(src.Remove(0, src.IndexOf('{'))));
+
+            return result;
+        }
+
+        public void FindFunctions(string src)
+        {
+            List<int> indexes = new List<int>();
+            int priority = 0;
+            int i = 0;
+            foreach ( var lit in src)
+            {
+                if (lit == '{')
+                {
+                    priority++;
+                    if (priority == 2)
+                    {
+                        string reversed = new string(src.Remove(i).Reverse().ToArray());
+                        if ( reversed.IndexOf(")") < 32)
+                        {
+                            indexes.Add(i);
+                        }
+                        
+
+                    }
+                }
+                if (lit == '}')
+                {
+                    priority--;
+                }
+                i++;
+            }
+            foreach ( var index in indexes)
+            {
+                var currentString = src.Remove(index);
+                var delimiterstr =  currentString.Split('(');
+                var delimiterstrfinaly = delimiterstr[delimiterstr.Count() - 2];
+                delimiterstr = delimiterstrfinaly.Split(' ');
+
+                currentString = NextTextInBrakes(src.Remove(0, index-3));
+
+                Functions.Add(new Function() {NameOfFunction = delimiterstr[delimiterstr.Count()-1], Source = currentString,
+                    McCabe = CalculateMcCabeNumber(currentString), Meyer = CalculateMeyersNumber(currentString) });        
+            }
+        }
+        public int CalculateMcCabeNumber(string body)
+        {
+            int result = 1;
+            Regex ifPattern = new Regex(@"if |if\(", RegexOptions.IgnoreCase);
+            int a = ifPattern.Matches(body).Count;
+            result += ifPattern.Matches(body).Count;
+
+            Regex asksignpattern = new Regex(@"\?");
+            result += asksignpattern.Matches(body).Count;
+
+            Regex casePattern = new Regex("case ", RegexOptions.IgnoreCase);
+            result += casePattern.Matches(body).Count;
+
+            Regex forPattern = new Regex(@"for |for\(", RegexOptions.IgnoreCase);
+            result += forPattern.Matches(body).Count;
+
+            Regex whilePattern = new Regex(@"while |for\(", RegexOptions.IgnoreCase);
+            result += whilePattern.Matches(body).Count;
+            return result;
+        }
+        public int CalculateMeyersNumber(string body)
+        {
+            int result = 1;
+            Regex Predicates = new Regex(@"( and)|( or)|(&&)|(\|\|)");
+            List<int> indexes = new List<int>(); 
+
+            Regex asksignpattern = new Regex(@"\?");
+            result += asksignpattern.Matches(body).Count;
+
+            Regex casePattern = new Regex("case ", RegexOptions.IgnoreCase);
+            result += casePattern.Matches(body).Count;
+
+            Regex ifPattern = new Regex(@"if |if\(", RegexOptions.IgnoreCase);
+            for (int i = 0; i < ifPattern.Matches(body).Count; i++)
+            {
+                indexes.Add(ifPattern.Matches(body)[i].Index);
+            }
+
+
+            Regex forPattern = new Regex(@"for |for\(", RegexOptions.IgnoreCase);
+            for (int i = 0; i < forPattern.Matches(body).Count; i++)
+            {
+                indexes.Add(forPattern.Matches(body)[i].Index);
+            }
+
+            Regex whilePattern = new Regex(@"while|while\(", RegexOptions.IgnoreCase);
+            for (int i = 0; i < whilePattern.Matches(body).Count; i++)
+            {
+                indexes.Add(whilePattern.Matches(body)[i].Index);
+            }
+            foreach (var i in indexes)
+            {
+                result += 1 + Predicates.Matches(TextInBrakets(body.Remove(0, i))).Count;
+            }
+
+            return result;
+
+        }
+        private string TextInBrakets(string body)
+        {
+
+            string result = body.Remove(0, body.IndexOf('(') + 1).Remove(1 + EndPosition(body.Remove(0, body.IndexOf('('))));
+            return result;
+        }
+
+        private int EndPosition(string src)
+        {
+            int counter = 0;
+            int result = 0;
+            for (int i = 0; (i < src.Length); i++)
+            {
+                if (src[i] == '(')
+                {
+                    counter++;
+                }
+                if (src[i] == ')')
+                {
+                    counter--;
+                    if (counter == 0)
+                    {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+
+    }
+    
+}
---
+++ MACCABE_JAVA/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MACCABE_JAVA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(375, 280);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(496, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(375, 167);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Из файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Анализ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 293);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
